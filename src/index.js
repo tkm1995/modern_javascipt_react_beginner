c5841dc973bb4de7a730fc6f3d913d2b39123d61
tkm1995 <73624675+tkm1995@users.noreply.github.com>
2021-09-20 17:34:14 +0900

    20210919 sec04 17~19

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-import "./styles.css";
+/**
+ * const, let等の変数宣言
+ */
+// var val1 = "hello";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+// val1 = "apple";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "orange";
+// console.log(val1);
+
+let val2 = "let param";
+console.log(val2);
+
+// letは再宣言不可能
+// let val2 = "let param2";
+
+// constは、オブジェクトのときは上書き可能
+// const var4 = {
+//   name: "taro",
+//   age: "25"
+// };
+// var4.name = "jiro";
+// var4.address = "tokyo";
+// console.log(var4);
+
+// constは、配列のときは上書き可能
+// const var5 = ["dog", "cat"];
+// var5[0] = "bird";
+// var5.push("monkey");
+// console.log(var5);
+
+/**
+ * テンプレート文字列
+ */
+const name = "mike";
+const age = "12";
+
+// 「私の名前はmikeです。１２歳です。」
+const msg = `私の名前は${name}です。${age}歳です。`;
+console.log(msg);
